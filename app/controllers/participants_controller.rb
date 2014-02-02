ac70a9ed1677c7bb08df8dc97b0696b244649769
Adriana Vecchioli <adriana_10_09@192.168.1.4>
2014-02-01 22:43:36 -0500

    update participants controller

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -1,22 +1,23 @@
 class ParticipantsController < ApplicationController
-def create
-    @event = Event.find(params[:event_id])
-    @participant = @event.participants.new(participant_params)
-    if @participant.save
-      redirect_to event_url(@event)
-    else
-      redirect_to event_url(@event)
+    def create
+        @event = Event.find(params[:event_id])
+        @participant = @event.participants.new(participant_params)
+        if @participant.save
+          redirect_to event_url(@event)
+        else
+          redirect_to event_url(@event)
+        end
     end
-end
 
-def participant_params
-		params[:participant].permit(:name)
-end
+    def participant_params
+    		params[:participant].permit(:name)
+    end
 
-def add_guest
-    @event = Event.find(params[:event_id])
-    @participant = Participant.find(params[:id])
-    @participant.guests += 1
-    @participant.save
-    redirect_to event_url(@event)
+    def add_guest
+        @event = Event.find(params[:event_id])
+        @participant = Participant.find(params[:id])
+        @participant.guests += 1
+        @participant.save
+        redirect_to event_url(@event)
+    end
 end
